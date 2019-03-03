3ea5497626f0985f4527a997a99564a5f289a8c8
Andrew Brampton <bramp@google.com>
2019-03-03 11:40:03 -0800

    Formatted the code with `php-cs-fixer fix`, and minor other changes.

--- crc32_benchmark.php
+++ crc32_benchmark.php
@@ -3,79 +3,86 @@ declare(strict_types=1);
 
 require('crc32.php');
 
-define('duration', 10);
-define('chunk', 1024 * 8);
+define('duration', 10);    // Duration of test in seconds.
+define('chunk', 1024 * 8); // Size of chunk to read from disk.
 
-interface Benchmark {
+interface Benchmark
+{
     public function init();
     public function update($buf);
     public function finish();
 }
 
-class Native implements Benchmark {
-	private $crc;
+class Native implements Benchmark
+{
+    private $crc;
 
-	public function init() {
-		$this->crc = hash_init("crc32b");
-	}
-	public function update($buf) {
-		hash_update($this->crc, $buf);
-	}
-	public function finish() {
-		return hash_final($this->crc);
-	}
+    public function init()
+    {
+        $this->crc = hash_init('crc32b');
+    }
+    public function update($buf)
+    {
+        hash_update($this->crc, $buf);
+    }
+    public function finish()
+    {
+        return hash_final($this->crc);
+    }
 }
 
-class PurePHP implements Benchmark {
-	private $crc32b;
+class PurePHP implements Benchmark
+{
+    private $crc;
 
-	public function init() {
-		$this->crc = new CRC32(CRC32::IEEE);
-	}
-	public function update($buf) {
-		$this->crc->update($buf);
-	}
-	public function finish() {
-		return $this->crc->hash();
-	}
+    public function init()
+    {
+        $this->crc = new CRC32(CRC32::IEEE);
+    }
+    public function update($buf)
+    {
+        $this->crc->update($buf);
+    }
+    public function finish()
+    {
+        return $this->crc->hash();
+    }
 }
 
-//xdebug_start_trace();
+function test($name, $test)
+{
+    $fp = fopen('/dev/urandom', 'rb');
+    if ($fp === false) {
+        exit("failed to open file");
+    }
 
-function test($name, $test) {
-	$fp = fopen('/dev/urandom', 'rb');
-	if ($fp === FALSE) {
-		exit("failed to open file");
-	}
+    $test->init();
 
-	$test->init();
+    //xdebug_start_trace();
 
-	$now = microtime(true);
-	$start = $now;
-	$offset = 0;
+    $now = microtime(true);
+    $start = $now;
+    $offset = 0;
 
-	while(($now - $start) < duration) {
-		$buf = fread($fp, chunk);
-		if ($buf === FALSE) {
-			exit("failed to read file");
-		}
+    while (($now - $start) < duration) {
+        $buf = fread($fp, chunk);
+        if ($buf === false) {
+            exit("failed to read file");
+        }
 
-		$test->update($buf);
-		$offset += strlen($buf);
+        $test->update($buf);
+        $offset += strlen($buf);
 
-		$now = microtime(true);
-	}
+        $now = microtime(true);
+    }
 
-	$test->finish();
+    $test->finish();
 
-	echo sprintf("%s %0.2f MB/s\n", $name, $offset / ($now - $start) / 1000000);
+    echo sprintf("%s %0.2f MB/s\n", $name, $offset / ($now - $start) / 1000000);
 
-	fclose($fp);
+    fclose($fp);
 }
 
-
-//$test = new Native();  // 12.36 MB/s
-$test = new PurePHP(); // 6.20 MB/s
-
-test('native', new Native());
-test('purephp', new PurePHP());
+                                // Tested on my mid-2014 MacBook Pro
+test('native', new Native());   // 12.36 MB/s
+test('purephp', new PurePHP()); // 6.20 MB/s
--- crc32_test.php
+++ crc32_test.php
@@ -4,37 +4,37 @@ declare(strict_types=1);
 require('crc32.php');
 
 $tests = array(
-	CRC32::IEEE => array(
-		'' => '00000000',
-		'a' => 'e8b7be43',
-		'abc' => '352441c2',
-		'message digest' => '20159d7f',
-		'abcdefghijklmnopqrstuvwxyz' => '4c2750bd',
-		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' => '1fc2e6d2',
-		'12345678901234567890123456789012345678901234567890123456789012345678901234567890' => '7ca94a72',
-	),
-	CRC32::Castagnoli => array(
-		'' => '00000000',
-		'a' => 'c1d04330',
-		'abc' => '364b3fb7',
-		'message digest' => '02bd79d0',
-		'abcdefghijklmnopqrstuvwxyz' => '9ee6ef25',
-		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' => 'a245d57d',
-		'12345678901234567890123456789012345678901234567890123456789012345678901234567890' => '477a6781',
-	),
+    CRC32::IEEE => array(
+        '' => '00000000',
+        'a' => 'e8b7be43',
+        'abc' => '352441c2',
+        'message digest' => '20159d7f',
+        'abcdefghijklmnopqrstuvwxyz' => '4c2750bd',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' => '1fc2e6d2',
+        '12345678901234567890123456789012345678901234567890123456789012345678901234567890' => '7ca94a72',
+    ),
+    CRC32::Castagnoli => array(
+        '' => '00000000',
+        'a' => 'c1d04330',
+        'abc' => '364b3fb7',
+        'message digest' => '02bd79d0',
+        'abcdefghijklmnopqrstuvwxyz' => '9ee6ef25',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' => 'a245d57d',
+        '12345678901234567890123456789012345678901234567890123456789012345678901234567890' => '477a6781',
+    ),
 );
 
 foreach ($tests as $poly => $cases) {
-	$crc = new CRC32($poly);
+    $crc = new CRC32($poly);
 
-	foreach ($cases as $input => $expected) {
-		$crc->update($input);
-		$actual = $crc->hash();
+    foreach ($cases as $input => $expected) {
+        $crc->update($input);
+        $actual = $crc->hash();
 
-		if ($actual !== $expected) {
-			echo "fail: crc32($poly).hash($input) = $actual wanted $expected\n";
-		}
+        if ($actual !== $expected) {
+            echo "fail: crc32($poly).hash($input) = $actual wanted $expected\n";
+        }
 
-		$crc->reset();
-	}
+        $crc->reset();
+    }
 }
